c4065bb4f339204700e9c700e587e79c5c99660d
shiryu <621nua@gmail.com>
2024-04-13 19:33:15 +0300

    adding a map and character

---
+++ 2d-farm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Sprite forwardSprite;
+    public Sprite backwardSprite;
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        // Установили компонент, который выполнится хоть раз
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // объявили кнопки для нажатий
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // установили взаимодействие, сразу через 3 вектор.
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+        if (horizontalInput > 0)
+        {
+            spriteRenderer.sprite = rightSprite;
+        }
+        else if (horizontalInput < 0)
+        {
+            spriteRenderer.sprite = leftSprite;
+        }
+        else if (verticalInput > 0)
+        {
+            spriteRenderer.sprite = forwardSprite;
+        }
+        else if (verticalInput < 0)
+        {
+            spriteRenderer.sprite = backwardSprite;
+        }
+    }
+}
