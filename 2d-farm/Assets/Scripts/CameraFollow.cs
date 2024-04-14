70cd9f6f90465b648b87997a1dbbb711857025d9
shiryu <621nua@gmail.com>
2024-04-14 12:53:22 +0300

    adding a camera that follows the character

---
+++ 2d-farm/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // Цель, за которой будет следовать камера
+    public float followSpeed = 5f;
+
+    void Update()
+    {
+        //transform.position = target.position + new Vector3(0, 0, -10); // не плавное, будет дергание.
+        transform.position = Vector3.Lerp(transform.position, target.position + new Vector3(0, 0, -10), followSpeed * Time.deltaTime);
+    }
+}
